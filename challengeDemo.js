69c39cb0fbfaf11b6c95ecc789fb268b13f78110
Pedro Dias <aluno17108@ipt.pt>
2020-02-20 20:51:24 +0000

    challenges 2

--- challengeDemo.js
+++ challengeDemo.js
@@ -30,24 +30,23 @@ function sortArray(array) {
   );
 }
 
-console.time('solution1');
-solution1([1, 3, 6, 4, 1, 2,1, 3, 6, 4, 1, 2]);
-solution1([1, 2, 3, 3, 6, 4, 1, 2,1, 3, 6, 4]);
+console.time("solution1");
+solution1([1, 3, 6, 4, 1, 2, 1, 3, 6, 4, 1, 2]);
+solution1([1, 2, 3, 3, 6, 4, 1, 2, 1, 3, 6, 4]);
 solution1([1, 2, 3]);
 solution1([-1, -3]);
-console.timeEnd('solution1');
-
+console.timeEnd("solution1");
 
 function solution2(A) {
   // write your code in JavaScript (Node.js 8.9.4)
   const filteredSet = new Set(A);
   const updatedArray = sortArray2([...filteredSet]);
 
-//   const filteredSet = A.reduce(
-//     (array, el) => (array.includes(el) ? array : [...array, el]),
-//     []
-//   );
-// const updatedArray = sortArray2(filteredSet);
+  //   const filteredSet = A.reduce(
+  //     (array, el) => (array.includes(el) ? array : [...array, el]),
+  //     []
+  //   );
+  // const updatedArray = sortArray2(filteredSet);
 
   const max = 100000;
   for (let i = 1; i < max; i++) {
@@ -60,40 +59,24 @@ function solution2(A) {
 
 function sortArray2(array) {
   let sorted;
-  let temp = null
-  while(!sorted){
-    sorted= true;
-    array.forEach((el,index) =>{
+  let temp = null;
+  while (!sorted) {
+    sorted = true;
+    array.forEach((el, index) => {
       temp = el;
-      if(array[index + 1] < el){
+      if (array[index + 1] < el) {
         el = array[index + 1];
         array[index + 1] = temp;
         sorted = false;
       }
-    })
+    });
   }
   return array;
-
-
-  // const len = array.length;
-  // for (let i = len - 1; i >= 0; i--) {
-  //   for (let j = 1; j <= i; j++) {
-  //     if (array[j - 1] > array[j]) {
-  //       let temp = array[j - 1];
-  //       array[j - 1] = array[j];
-  //       array[j] = temp;
-  //     }
-  //   }
-  //   console.log(array)
-  // }
-  // return array;
 }
 
-
-console.time('solution2');
-solution2([1, 3, 6, 4, 1, 2,1, 3, 6, 4, 1, 2]);
-solution2([1, 2, 3, 3, 6, 4, 1, 2,1, 3, 6, 4]);
+console.time("solution2");
+solution2([1, 3, 6, 4, 1, 2, 1, 3, 6, 4, 1, 2]);
+solution2([1, 2, 3, 3, 6, 4, 1, 2, 1, 3, 6, 4]);
 solution2([1, 2, 3]);
 solution2([-1, -3]);
-console.timeEnd('solution2');
-
+console.timeEnd("solution2");
---
+++ ex_example.js
@@ -0,0 +1,118 @@
+// console.log("1" == 1);
+// // true
+// console.log("1" != 1);
+// // false
+// console.log("1" === 1);
+// // false
+// console.log("1" !== 1);
+// // true
+// console.log("10" + 1);
+// // 101
+// console.log("10" - 1);
+// // 9
+// console.log(true && 10 && 11 && 9);
+// // 9
+// console.log(true || 10 || 11 || 9);
+// // 9
+// console.log(!true || 0 || 11 || 9);
+// // true
+// console.log("b" + "a" + +"a" + "a");
+// // baNaNa
+// console.log({} === {});
+// // false
+// console.log({} == {});
+// // false
+// console.log(null - 5);
+// // -5
+// console.log(undefined - 5);
+// // NaN
+
+// ------------
+// Explain the differences
+// var - function scoped and gets hoisted
+// let  - block scoped
+// const  - doesnt let value change
+
+// ------------
+// When and what will this function log?
+// Can you fix this to output 0 - 1 - 2 - 3 - 4 separated by 1s per log?
+
+// for (let i = 0; i < 5; i++) {
+//   setTimeout(function() {
+//     console.log(i);
+//   }, 1000);
+// }
+
+// 5 , 5 , 5 , 5 , 5 (after 1 sec) because of scope/hoisting
+// change "var i" to "let i"
+
+// --------------
+// What's the output of the following snippet?
+
+// const a = { c: 2, d: 4 };
+// const b = a; // passed reference
+// b.c = 3;
+// console.log(a.c);
+// // 3 because of object reference
+
+// --------------
+// Write a js function that would allow this with the following output
+
+// "Penny".sheldonize(3);
+// // Knock, knock, knock Penny! Knock, knock, knock Penny! Knock, knock, knock Penny!
+
+// String.prototype.sheldonize = function(val) {
+//   let output = "Knock, knock, knock " + this.valueOf() + "! ";
+//   if (val === 0) {
+//     return "";
+//   }
+//   for (let i = 1; i < val; i++) {
+//     output = output + "Knock, knock, knock " + this.valueOf() + "! ";
+//   }
+//   return output;
+// };
+
+// console.log("Penny".sheldonize(3));
+
+// --------------
+// Imagine that you have a gigantic array and you want to extract values to an array with the following conditions
+// - evens
+// - positives
+// - uniques
+// - sorted
+// Write a function that would allow you to achieve this output
+
+// const array = [1, -2, 5, -10, 14, "11", 9, 24, 2, 4, "21" /*,(...)*/];
+
+// function filterArray(arr) {
+//   const filteredArray = arr.reduce(
+//     (array, el) =>
+//       el % 2 !== 0 && el >= 1 && !array.includes(el) ? [...array, +el] : array,
+//     []
+//   );
+//   console.log("Filtered");
+//   console.log(filteredArray);
+//   const sortedArray = sortArray(filteredArray);
+//   console.log("Sorted");
+//   console.log(sortedArray);
+// }
+
+// function sortArray(arr) {
+//   let sorted = false;
+//   let temp = null;
+//   while (!sorted) {
+//     sorted = true;
+//     arr.forEach((el, index) => {
+//       temp = arr[index];
+//       if (temp > arr[index + 1]) {
+//         arr[index] = arr[index + 1];
+//         arr[index + 1] = temp;
+//         sorted = false;
+//       }
+//     });
+//   }
+//   return arr;
+// }
+// console.time("filterArray");
+// filterArray(array);
+// console.timeEnd("filterArray");
